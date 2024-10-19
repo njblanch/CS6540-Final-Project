690529518c6fadc9534e8748342e9a12a05b43aa
Gian <gian@cercena.com>
2024-10-19 14:34:12 -0400

    fixed all_vis

--- 6_visual_features/all_vis.sh
+++ 6_visual_features/all_vis.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #SBATCH --account=cs6540
-#SBATCH --job-name=vis_feats_$1
-#SBATCH --output=log/vis_feats_output_$1.log
-#SBATCH --error=log/vis_feats_error_$1.log
+#SBATCH --job-name=vis_feats
+#SBATCH --output=log/vis_feats_output.log
+#SBATCH --error=log/vis_feats_error.log
 #SBATCH --ntasks=4
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=24GB
@@ -19,9 +19,12 @@ while IFS= read -r line; do
         continue
     fi
     
-    script_loc = "/gpfs2/classes/cs6540/AVSpeech/6_visual_features/visual_features.py"
+    script_loc="/gpfs2/classes/cs6540/AVSpeech/6_visual_features/visual_features.py"
 
-    /gpfs1/home/g/c/gcercena/miniconda3/envs/dl/bin/python $script_loc -i "$line"
+    video_src="/gpfs2/classes/cs6540/AVSpeech/2_unzipped/$line/$line/"
 
+    /gpfs1/home/g/c/gcercena/miniconda3/envs/dl/bin/python $script_loc -i $video_src &
 
 done < "$input_file"
+
+wait
