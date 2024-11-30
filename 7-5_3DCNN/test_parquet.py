6958eda2f521686b61bf18f7f8d1be2979662944
Gian <gian@cercena.com>
2024-11-30 15:36:35 -0500

    wlos model

---
+++ 7-5_3DCNN/test_parquet.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_parquet("0-0sOa1kk5c.parquet")
+print(df)
+print(df.columns)
+# Plot the histogram for the "clip_num" column
+plt.hist(df["clip_num"], bins=30, edgecolor="black")  # Adjust 'bins' as necessary
+plt.title("Histogram of clip_num")
+plt.xlabel("clip_num")
+plt.ylabel("Frequency")
+plt.show()
