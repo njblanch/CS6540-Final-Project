fixed variance regularization bug (ignores if NaN)

7-5_3DCNN/evaluate_model.py
@@ -170,7 +170,7 @@ def main():
 
     # Instantiate the model
     model = AudioVisualModel(num_mfcc_rows=fixed_num_mfcc_rows).to(device)
-    model_name = "audio_visual_sync_model_7_best.pth"
+    model_name = "audio_visual_sync_model_5_best.pth"
     model.load_state_dict(torch.load(model_name))
     logging.info("Model instantiated.")
 

7-5_3DCNN/model.py
@@ -494,7 +494,11 @@ def main():
 
                 # Variance loss
                 var_loss = variance_regularization(outputs, alpha=alph)
-                loss = mse_loss + var_loss # Full loss
+                if torch.isnan(var_loss):
+                    print("Warning: NaN value detected in variance regularization loss. Using only MSE loss.")
+                    loss = mse_loss
+                else:
+                    loss = mse_loss + var_loss # Full loss
                 loss.backward()
                 optimizer.step()
 